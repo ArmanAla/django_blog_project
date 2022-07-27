ef8a128972c56db05d49ba49aacf685b68ce022d
Ala9921160003 <90964584+Ala9921160003@users.noreply.github.com>
2022-07-27 16:56:30 +0430

    In views.py, inside startin_page(), we are recieving data from database.

--- blog/views.py
+++ blog/views.py
@@ -13,8 +13,7 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html",{
         "posts" : latest_posts
     })
