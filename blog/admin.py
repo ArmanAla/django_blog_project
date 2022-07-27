ea5dec03a3cb90c30f0449fb75ffab8aa241d614
Ala9921160003 <90964584+Ala9921160003@users.noreply.github.com>
2022-07-27 15:57:47 +0430

    simple form of admin panel is created.

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import Tag, Post, Author
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
