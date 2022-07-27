deaafbed7bd3288b104e24458306c704c1475f60
Ala9921160003 <90964584+Ala9921160003@users.noreply.github.com>
2022-07-27 16:31:58 +0430

    list_display was implemented for PostAdmin class.

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,9 @@ from .models import Tag, Post, Author
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "author")
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
